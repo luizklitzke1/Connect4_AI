369db7cbc450326bae86a7943c0b14a550591250
luizklitzke1 <luiz.klitzke1@gmail.com>
2023-10-22 23:43:23 -0300

    Base para o funcionamento da IA, falta encontrar um heuristica boa

--- agenteIA.py
+++ agenteIA.py
@@ -1,12 +1,88 @@
+import math
+
 from agente import Agente
 from tabuleiro import Tabuleiro
 
-class AgenteIA(Agente):
+INFINITO_POSITIVO =  math.inf
+INFINITO_NEGATIVO = -math.inf
 
-    def jogar(self, tabuleiro):
-        return super().jogar(tabuleiro)
-    
+PLAYER_1 = 1
+PLAYER_2 = 2
+
+class AgenteIA(Agente):
+    #Define o valor de um determinado estado do tabuleiro
+    def calculaHeuristica(self, tabuleiro, agente):
+        matriz = tabuleiro.getMatriz()
+
+        return 99        
 
     #"Algorithms Explained – minimax and alpha-beta pruning" - https://www.youtube.com/watch?v=l-hh51ncgDI&ab_channel=SebastianLague
-    def buscaColunaMinMax(self, tabuleiro, profundidade, playerMaximizar):
-        pass
+    def buscaColunaMinMax(self, tabuleiro, profundidade, alpha, beta, playerMaximizar):
+        colunasLivres = tabuleiro.getListaColunasLivres()
+
+        idAdversario = PLAYER_2 if self.getId() ==  PLAYER_1 else PLAYER_1 
+
+        vitoriaAgente = tabuleiro.verificarVitoria(self.getId())
+        vitoriaAdversario = tabuleiro.verificarVitoria(idAdversario)
+
+        posicaoTerminal = len(colunasLivres) == 0 or vitoriaAgente or vitoriaAdversario
+
+        if (profundidade == 0 or posicaoTerminal):
+
+            if posicaoTerminal:
+                if vitoriaAgente:
+                    return (None, INFINITO_POSITIVO)
+                elif vitoriaAdversario:
+                    return (None, INFINITO_NEGATIVO)
+                else:
+                    return (None, 0) #Nenhuma jogada válida
+                
+            else: #Profundidade zero == ultimo nó possivel de expandir
+                return (None, self.calculaHeuristica(tabuleiro, self.getId()))
+            
+        if playerMaximizar == idAdversario:
+            valorHeuristica = INFINITO_NEGATIVO
+            coluna = None
+
+            for coluna in colunasLivres:
+                #Copia auxilia do tabuleiro para avaliar as variações em recursivo
+                tabuleiroAux = tabuleiro.copy()
+                tabuleiroAux.posiciona(coluna, idAdversario)
+
+                colunaAux, heuristicaFilho = self.buscaColunaMinMax(tabuleiroAux, profundidade - 1, alpha, beta, self.getId())
+                if heuristicaFilho > valorHeuristica:
+                    valorHeuristica = heuristicaFilho
+                    coluna = colunaAux
+
+                alpha = max(alpha, valorHeuristica)
+
+                if beta <= alpha:
+                    break
+
+            return coluna, valorHeuristica
+
+        elif playerMaximizar == self.getId():
+            valorHeuristica = INFINITO_POSITIVO
+            coluna = None
+
+            for coluna in colunasLivres:
+                #Copia auxilia do tabuleiro para avaliar as variações em recursivo
+                tabuleiroAux = tabuleiro.copy()
+                tabuleiroAux.posiciona(coluna, self.getId())
+
+                colunaAux, heuristicaFilho = self.buscaColunaMinMax(tabuleiroAux, profundidade - 1, alpha, beta, idAdversario)
+                if heuristicaFilho > valorHeuristica:
+                    valorHeuristica = heuristicaFilho
+                    coluna = colunaAux
+
+                beta = min(beta, valorHeuristica)
+
+                if beta <= alpha:
+                    break
+
+            return coluna, valorHeuristica
+        
+    def jogar(self, tabuleiro):
+        coluna, valorMinMax = self.buscaColunaMinMax(tabuleiro, tabuleiro.getLinhas() - 1, INFINITO_NEGATIVO, INFINITO_POSITIVO, self.getId())
+
+        tabuleiro.posiciona(coluna, self.getId())
--- tabuleiro.py
+++ tabuleiro.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+LINHA_INVALIDA = -1
+
 class Tabuleiro():
     
     def __init__(self, linhas, colunas):
@@ -30,28 +32,45 @@ class Tabuleiro():
         for linha in range(self.getLinhas()):
             print(self.getMatriz()[linha])
 
-    #Posiciona uma peça do player no primeiro espaço livre, de baixo para cima, da coluna
-    def posiciona(self, coluna, player):
+    #Primeira linha livre de baixo para cima na coluna. Caso nenhuma, retorna LINHA_INVALIDA
+    def getPosicaoLivreColuna(self, coluna):
         for linha in range(self.getLinhas()):
             if (self.getMatriz()[self.getLinhas() - 1 - linha][coluna] == 0):
-                self.getMatriz()[self.getLinhas() - 1 - linha][coluna] = player
-                return True
+                return linha
             
-        return False #Coluna inválida
+        return LINHA_INVALIDA #Coluna completamente preenchida
+    
+    #Retorna todas as colunas ainda válidas
+    def getListaColunasLivres(self):
+        colunasLivres = []
 
-    #Retorna true caso o player tenha vencido com o ultimo movimento
-    def verificarVitoria(self, player):
+        for coluna in range(self.getColunas()):
+            if (self.getPosicaoLivreColuna(coluna) != LINHA_INVALIDA):
+                colunasLivres.insert(coluna)
+
+    #Posiciona uma peça do agente no primeiro espaço livre, de baixo para cima, da coluna
+    def posiciona(self, coluna, agente):
+        linhaPosicionar = self.getPosicaoLivreColuna(coluna)
+
+        if linhaPosicionar == LINHA_INVALIDA:
+            return False #Coluna inválida
+        
+        self.getMatriz()[linhaPosicionar][coluna] = agente.getId()
+            
+
+    #Retorna true caso o agente tenha vencido com o ultimo movimento
+    def verificarVitoria(self, agente):
 
         #Quatro peças na horizontal
         for linha in range(self.getLinhas()):
             for coluna in range(self.getColunas() - 3):
-                if (self.getMatriz()[linha][coluna] == player and self.getMatriz()[linha][coluna + 1] == player and self.getMatriz()[linha][coluna + 2] == player and self.getMatriz()[linha][coluna + 3] == player):
+                if (self.getMatriz()[linha][coluna] == agente and self.getMatriz()[linha][coluna + 1] == agente and self.getMatriz()[linha][coluna + 2] == agente and self.getMatriz()[linha][coluna + 3] == agente):
                     return True
 
         #Quatro peças na vertical
         for coluna in range(self.getColunas()):
             for linha in range(self.getLinhas() - 3):
-                if (self.getMatriz()[linha][coluna] == player and self.getMatriz()[linha + 1][coluna] == player and self.getMatriz()[linha + 2][coluna] == player and self.getMatriz()[linha + 3][coluna] == player):
+                if (self.getMatriz()[linha][coluna] == agente and self.getMatriz()[linha + 1][coluna] == agente and self.getMatriz()[linha + 2][coluna] == agente and self.getMatriz()[linha + 3][coluna] == agente):
                     return True
                 
         #Quatro peças em uma diagonal
@@ -59,11 +78,11 @@ class Tabuleiro():
             for linha in range(self.getLinhas() - 3):
                 
                 #Superior esquerdo para inferior direito
-                if (self.getMatriz()[linha][coluna] == player and self.getMatriz()[linha + 1][coluna +1] == player and self.getMatriz()[linha + 2][coluna + 2] == player and self.getMatriz()[linha + 3][coluna + 3] == player):
+                if (self.getMatriz()[linha][coluna] == agente and self.getMatriz()[linha + 1][coluna +1] == agente and self.getMatriz()[linha + 2][coluna + 2] == agente and self.getMatriz()[linha + 3][coluna + 3] == agente):
                     return True
                 
                 #Inferior esquerdo para superior direito
-                if (self.getMatriz()[self.getLinhas() - linha - 1][self.getColunas() - coluna - 1] == player and self.getMatriz()[self.getLinhas() - linha - 2][self.getColunas() - coluna - 2] == player and self.getMatriz()[self.getLinhas() - linha - 3][self.getColunas() - coluna - 3] == player and self.getMatriz()[self.getLinhas() - linha - 4][self.getColunas() - coluna - 4] == player):
+                if (self.getMatriz()[self.getLinhas() - linha - 1][self.getColunas() - coluna - 1] == agente and self.getMatriz()[self.getLinhas() - linha - 2][self.getColunas() - coluna - 2] == agente and self.getMatriz()[self.getLinhas() - linha - 3][self.getColunas() - coluna - 3] == agente and self.getMatriz()[self.getLinhas() - linha - 4][self.getColunas() - coluna - 4] == agente):
                     return True
                 
         return False
